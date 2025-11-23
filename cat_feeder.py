e5decfc5fafca0bc91e5b6853fb598625eaac556
pozdeeva_ksu <78385823+kirikova-k@users.noreply.github.com>
2025-11-24 01:02:48 +0300

    Add files via upload

---
+++ cat_feeder.py
@@ -0,0 +1,52 @@
+import asyncio
+from dotenv import find_dotenv, load_dotenv
+from langchain_gigachat import GigaChat
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from rich import print as rprint
+
+model = GigaChat(model="GigaChat-2-Max",
+                credentials="NzMxZWJkNzktMDkwMy00ZTI2LWFiMGQtNmVjMjBjZmJmYWI2OjU0OGY2ZDI5LTM3ZmUtNGZhZS04MWY3LWY2YzkyNTc0YTU3Nw==",
+                verify_ssl_certs=False,
+                streaming=False,
+                max_tokens=8000,
+                timeout=600)
+
+#копипаста логгера из гайда
+def _log(ans):
+    for message in ans['messages']:
+        rprint(f"[{type(message).__name__}] {message.content} {getattr(message, 'tool_calls', '')}")
+
+
+async def main():
+    server_params = StdioServerParameters(
+        command="python",
+        args=["server.py"], #название файла с тулзами
+    )
+
+    async with stdio_client(server_params) as (read, write):
+        #I/O коннект
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+
+            # Get tools
+            tools = await load_mcp_tools(session)
+
+            prompt="Ты - умная кормушка для кота. Пользователь вводит вес кота. Если вес больше 5 ты гладишь кота" \
+            "Если вес меньше 5 ты кормишь кота"
+
+            #TODO: deprecated метод
+            agent = create_react_agent(model=model, tools=tools, prompt=prompt)
+            while True:
+                user_input = input("Вес кота: ")
+                if user_input.lower() == "exit":
+                    break    
+                #отправка сообщения ии агенту, agent_response - ответ
+                agent_response = await agent.ainvoke({"messages": [
+                    {"role": "user", "content": "Вес кота:" + user_input}]})
+                _log(agent_response)
+            
+# Run the main function
+asyncio.run(main())
---
+++ server.py
@@ -0,0 +1,22 @@
+from mcp.server.fastmcp import FastMCP
+from pydantic import BaseModel
+import sys
+
+mcp = FastMCP("CatFeeder")
+
+class Query(BaseModel):
+    query: str
+
+@mcp.tool()
+def feed_cat(weight: float) -> float:
+    """Feed Cat"""
+    print(f">>> Feeding cat {weight} food)")
+    return weight + 0.3
+
+@mcp.tool()
+def pet_cat():
+    """Pet cat"""
+    return
+if __name__ == "__main__":
+    transport = sys.argv[1] if len(sys.argv) > 1 else "stdio"
+    mcp.run(transport=transport)
