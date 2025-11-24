e79d61d66522b86de32a76dc5262658898f3f26b
kirikova-k <kirickova.k@yandex.ru>
2025-11-24 11:35:58 +0300

    edited prompt

--- server.py
+++ server.py
@@ -10,13 +10,13 @@ class Query(BaseModel):
 @mcp.tool()
 def feed_cat(weight: float) -> float:
     """Feed Cat"""
-    print(f">>> Feeding cat {weight} food)")
+    print(f">>> Feeding cat {weight} food")
     return weight + 0.3
 
 @mcp.tool()
 def pet_cat():
     """Pet cat"""
-    return
+    return "Pet cat"
 if __name__ == "__main__":
     transport = sys.argv[1] if len(sys.argv) > 1 else "stdio"
     mcp.run(transport=transport)
